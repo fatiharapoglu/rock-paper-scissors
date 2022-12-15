9055567f1e71d250a3788085837fb6f96e1063be
Fatih Arapoglu <f3tt4n@gmail.com>
2022-12-16 01:45:32 +0300

    prettier

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ const btnRock = document.querySelector("#rock");
 const btnPaper = document.querySelector("#paper");
 const btnScissors = document.querySelector("#scissors");
 const text = document.querySelector("#text");
-const playOneMore = document.querySelector(".box")
+const playOneMore = document.querySelector(".box");
 const comp = document.querySelector("#comp");
 const human = document.querySelector("#human");
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 const modalWin = document.getElementById("uWin");
 const modalLose = document.getElementById("uLose");
-const spanWin = document.querySelector("#closeWin");
-const spanLose = document.querySelector("#closeLose");
-const rockSFX = new Audio('./assets/rock.mp3');
-const paperSFX = new Audio('./assets/paper.mp3');
-const scissorsSFX = new Audio('./assets/scissors.mp3');
+const spanWin = document.querySelector("#close-win");
+const spanLose = document.querySelector("#close-lose");
+const rockSFX = new Audio("./assets/rock.mp3");
+const paperSFX = new Audio("./assets/paper.mp3");
+const scissorsSFX = new Audio("./assets/scissors.mp3");
 let tie;
 let computerWins;
 let playerWins;
@@ -22,16 +22,15 @@ let playerScore = 0;
 let computerScore = 0;
 rockSFX.volume = 0.2;
 
-
 // Modal settings start
 function openModalWin() {
     modalWin.style.display = "block";
-};
+}
 
-spanWin.onclick = function() {
+spanWin.onclick = function () {
     modalWin.style.display = "none";
     const playAgain = document.createElement("p");
-    playAgain.innerHTML = `<a href="index.html" class="playAgain">Play Again?</a>` ;
+    playAgain.innerHTML = `<a href="index.html" class="play-again">Play Again?</a>`;
     playOneMore.appendChild(playAgain);
 };
 
@@ -39,191 +38,141 @@ function openModalLose() {
     modalLose.style.display = "block";
 };
 
-spanLose.onclick = function() {
+spanLose.onclick = function () {
     modalLose.style.display = "none";
     const playAgain = document.createElement("p");
-    playAgain.innerHTML = `<a href="index.html" class="playAgain">Play Again?</a>` ;
+    playAgain.innerHTML = `<a href="index.html" class="play-again">Play Again?</a>`;
     playOneMore.appendChild(playAgain);
 };
-// Modal settings end
 
-
-// Disabling buttons function 
+// Disabling buttons function
 function disableButtons() {
-    buttons.forEach(elem => {
-        elem.disabled = true
-    })
+    buttons.forEach((item) => {
+        item.disabled = true;
+    });
 };
 
-
 // Buttonworks / rock
-btnRock.addEventListener('click', () => {
-    
-    playerChoice = "rock" ;
+btnRock.addEventListener("click", () => {
+    playerChoice = "rock";
     rockSFX.play();
-    console.log(playerChoice) ;
     result = playRound();
     const pResult = document.createElement("p");
-    pResult.innerHTML = `${result} \n ` ;
+    pResult.innerHTML = `${result} \n `;
     text.appendChild(pResult);
-
     if (result == computerWins) {
         computerScore = computerScore + 1;
-    }
-
-    else if (result == playerWins) {
+    } else if (result == playerWins) {
         playerScore = playerScore + 1;
     }
-
-    comp.innerHTML = `${computerScore}`
-    human.innerHTML = `${playerScore}`
-
+    comp.innerHTML = `${computerScore}`;
+    human.innerHTML = `${playerScore}`;
     if (computerScore == 5) {
         computerScore = 0;
         playerScore = 0;
-        openModalLose()
-        disableButtons()
-    
-    }
-
-    else if (playerScore == 5 ) {
+        openModalLose();
+        disableButtons();
+    } else if (playerScore == 5) {
         computerScore = 0;
         playerScore = 0;
-        openModalWin()
-        disableButtons()
+        openModalWin();
+        disableButtons();
     }
 });
-
 
 // Buttonworks / paper
-btnPaper.addEventListener('click', () => {
-    
-    playerChoice = "paper" ;
+btnPaper.addEventListener("click", () => {
+    playerChoice = "paper";
     paperSFX.play();
-    console.log(playerChoice) ;
-    result = playRound()
+    result = playRound();
     const pResult = document.createElement("p");
-    pResult.innerHTML = `${result} \n ` ;
+    pResult.innerHTML = `${result} \n `;
     text.appendChild(pResult);
-
     if (result == computerWins) {
         computerScore = computerScore + 1;
-    }
-
-    else if (result == playerWins) {
+    } else if (result == playerWins) {
         playerScore = playerScore + 1;
     }
-
-
-    comp.innerHTML = `${computerScore}`
-    human.innerHTML = `${playerScore}`
-
+    comp.innerHTML = `${computerScore}`;
+    human.innerHTML = `${playerScore}`;
     if (computerScore == 5) {
         computerScore = 0;
         playerScore = 0;
-        openModalLose()
-        disableButtons()
-    
-    }
-
-    else if (playerScore == 5 ) {
+        openModalLose();
+        disableButtons();
+    } else if (playerScore == 5) {
         computerScore = 0;
         playerScore = 0;
-        openModalWin()
-        disableButtons()
+        openModalWin();
+        disableButtons();
     }
-
 });
-
 
 // Buttonworks / scissors
-btnScissors.addEventListener('click', () => {
-    
-    playerChoice = "scissors" ;
+btnScissors.addEventListener("click", () => {
+    playerChoice = "scissors";
     scissorsSFX.play();
-    console.log(playerChoice) ;
-    result = playRound()
+    result = playRound();
     const pResult = document.createElement("p");
-    pResult.innerHTML = `${result} \n ` ;
+    pResult.innerHTML = `${result} \n `;
     text.appendChild(pResult);
-
     if (result == computerWins) {
         computerScore = computerScore + 1;
-    }
-
-    else if (result == playerWins) {
+    } else if (result == playerWins) {
         playerScore = playerScore + 1;
     }
-
-    comp.innerHTML = `${computerScore}`
-    human.innerHTML = `${playerScore}`
-
+    comp.innerHTML = `${computerScore}`;
+    human.innerHTML = `${playerScore}`;
     if (computerScore == 5) {
         computerScore = 0;
         playerScore = 0;
-        openModalLose()
-        disableButtons()
-    
-    }
-
-    else if (playerScore == 5 ) {
+        openModalLose();
+        disableButtons();
+    } else if (playerScore == 5) {
         computerScore = 0;
         playerScore = 0;
-        openModalWin()
-        disableButtons()
+        openModalWin();
+        disableButtons();
     }
 });
-
 
 // Computer choice function
 const options = ["rock", "paper", "scissors"];
 
-function getComputerChoice() { 
-    let computerChoice = Math.floor(Math.random() * (options.length))
-    computerPlays = options[computerChoice]
-    return
-};
-
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * options.length);
+    computerPlays = options[computerChoice];
+}
 
 // One round function
 function playRound() {
-
-    getComputerChoice()
-
+    getComputerChoice();
     if (computerPlays == playerChoice) {
-        tie = "IT'S A TIE!"
+        tie = "IT'S A TIE!";
         return tie;
-    }
-
-    else if (computerPlays == "rock" && playerChoice == "scissors") {
-        computerWins = "Computer plays ROCK: <br>Rock beats scissors, you lost this round. "
+    } else if (computerPlays == "rock" && playerChoice == "scissors") {
+        computerWins =
+            "Computer plays ROCK: <br>Rock beats scissors, you lost this round. ";
         return computerWins;
-    }
-
-    else if (computerPlays == "scissors" && playerChoice == "rock") {
-        playerWins = "Computer plays SCISSORS: <br>Rock beats scissors, you win this round. "
+    } else if (computerPlays == "scissors" && playerChoice == "rock") {
+        playerWins =
+            "Computer plays SCISSORS: <br>Rock beats scissors, you win this round. ";
+        return playerWins;
+    } else if (computerPlays == "paper" && playerChoice == "rock") {
+        computerWins =
+            "Computer plays PAPER: <br>Paper beats rock, you lost this round.";
+        return computerWins;
+    } else if (computerPlays == "rock" && playerChoice == "paper") {
+        playerWins =
+            "Computer plays ROCK: <br>Paper beats rock, you win this round.";
+        return playerWins;
+    } else if (computerPlays == "scissors" && playerChoice == "paper") {
+        computerWins =
+            "Computer plays SCISSORS: <br>Scissors beats paper, you lost this round.";
+        return computerWins;
+    } else if (computerPlays == "paper" && playerChoice == "scissors") {
+        playerWins =
+            "Computer plays PAPER: <br>Scissors beats paper, you win this round.";
         return playerWins;
     }
-
-    else if (computerPlays == "paper" && playerChoice == "rock") {
-        computerWins = "Computer plays PAPER: <br>Paper beats rock, you lost this round."
-        return computerWins;
-    }  
-
-    else if (computerPlays == "rock" && playerChoice == "paper") {
-        playerWins = "Computer plays ROCK: <br>Paper beats rock, you win this round."
-        return playerWins;
-    }
-
-    else if (computerPlays == "scissors" && playerChoice == "paper") {
-        computerWins = "Computer plays SCISSORS: <br>Scissors beats paper, you lost this round."
-        return computerWins;
-    }
-
-    else if (computerPlays == "paper" && playerChoice == "scissors") {
-        playerWins = "Computer plays PAPER: <br>Scissors beats paper, you win this round."
-        return playerWins;
-    }    
-
-return
-};
+}
